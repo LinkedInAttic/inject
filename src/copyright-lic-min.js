bed392ce934b448eea4cf78925a46f13db5c0c3d
Matt Powell <mttpwll@gmail.com>
2011-11-28 14:44:58 -0800

    adds @license jsdoc annotations so the compiler doesn't strip the license block.

--- src/copyright-lic-min.js
+++ src/copyright-lic-min.js
@@ -1,5 +1,6 @@
-/*
-Inject (c) 2011 Jakob Heuser [https://github.com/jakobo/inject] Apache Software License 2.0
-lscache (c) 2011 Pamela Fox [https://github.com/pamelafox/lscache] Apache Software License 2.0
-Porthole (c) 2011 Ternary Labs [https://github.com/ternarylabs/porthole] MIT License
-*/
+/**
+ * @license
+ * Inject (c) 2011 Jakob Heuser [https://github.com/jakobo/inject] Apache Software License 2.0
+ * lscache (c) 2011 Pamela Fox [https://github.com/pamelafox/lscache] Apache Software License 2.0
+ * Porthole (c) 2011 Ternary Labs [https://github.com/ternarylabs/porthole] MIT License
+ */
