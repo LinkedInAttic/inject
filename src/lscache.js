0f56f41a1c7de5fdc2b18d27a4eb8dd8f50643cd
Jakob Heuser <jakob@felocity.com>
2011-12-09 17:44:53 -0800

    changed lscache implementation based on feedback from @mattpoweell. Relates to #14 with the localStorage shim

--- src/lscache.js
+++ src/lscache.js
@@ -1,4 +1,21 @@
 /**
+ * lscache library
+ * Copyright (c) 2011, Pamela Fox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * Creates a namespace for the lscache functions.
  */
 var lscache = function() {
@@ -107,7 +124,7 @@ var lscache = function() {
               storedKey = localStorage.key(i);
               if (storedKey.indexOf(TOUCHEDSUFFIX) > -1) {
                 var mainKey = storedKey.split(TOUCHEDSUFFIX)[0];
-                storedKeys.push({key: mainKey, touched: parseInt(localStorage[storedKey], 10)});
+                storedKeys.push({key: mainKey, touched: parseInt(localStorage.getItem(storedKey), 10)});
               }
             }
             storedKeys.sort(function(a, b) { return (a.touched-b.touched); });
