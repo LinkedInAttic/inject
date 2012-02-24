ac150813aa4f5bbcab00dcf5df646b77d4699dbf
Stephen <stephen.g.lynn+github@gmail.com>
2012-02-24 15:51:39 -0700

    Fix bug where an '=' or ',' character right before the require statement broke the loading of the module.
    There's probably a better way to solve this by tweaking the main regex
    that extracts the require statements from the script but this works and
    provides some tests to demonstrate the problem.

---
+++ tests/modules-1.0/includes/bugs/bug_require.js
@@ -0,0 +1,4 @@
+var b=require('bug_require_a');
+
+exports.foo = "bar";
+ok(true, "bug require loaded");
---
+++ tests/modules-1.0/includes/bugs/bug_require_a.js
@@ -0,0 +1,3 @@
+exports.foo = "bar";
+ok(true, "require statement with no space before require should have run");
+start();
