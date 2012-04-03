205ad93e3ce2ebd185883b127025ea62c664decf
chaozhang <czhang@linkedin.com>
2012-04-03 15:12:19 -0700

    clean up, for AMD testing of 2 level up relative path

--- tests/amd/includes/spec/anon/impl/array.js
+++ tests/amd/includes/spec/anon/impl/array.js
@@ -1,7 +1,8 @@
-define(['../util', './util'], function (util_ul, util_cl) {
+define(['../util', './util', '../../util'], function (util_ul, util_cl, util_uul) {
     return {
         name: 'impl/array',
         utilNameUl: util_ul.name,
-        utilNameCl: util_cl.name
+        utilNameCl: util_cl.name,
+        utilNameUUl: util_uul.name
     };
 });
