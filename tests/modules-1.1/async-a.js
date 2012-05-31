391e90c36c6099593fb615aa3457201e76c606e5
Jakob Heuser <jakob@felocity.com>
2012-05-31 16:54:40 -0700

    Updates #117 with test refinement. Removed async tests where not needed, and used raises() directly

--- tests/modules-1.1/async-a.js
+++ tests/modules-1.1/async-a.js
@@ -39,6 +39,20 @@ asyncTest("require.ensure", 3, function() {
   });
 });
 
+module("CommonJS: Modules 1.1 Extension - Async/A Bugs", {
+  setup: function() {
+    if (localStorage) {
+      localStorage.clear();
+    }
+    Inject.reset();
+  },
+  teardown: function() {
+    if (localStorage) {
+      localStorage.clear();
+    }
+  }
+});
+
 // require.ensure was running dependencies at compile time
 asyncTest("#58 require.ensure runtime dependencies only (false)", 1, function() {
   require.setModuleRoot("/tests/modules-1.1/includes/bugs");
@@ -53,5 +67,49 @@ asyncTest("#58 require.ensure runtime dependencies only (true)", 3, function() {
   require.ensure(["bug_58"], function(require) {
     var runner = require("bug_58");
     runner.runTest(true); // include subfile
+  });
+});
+
+asyncTest("#117 require.ensure() - if first argument (moduleList) is an Array, no exception should be thrown", 1, function() {
+  require.setModuleRoot('/tests/amd/includes/bugs');
+  require.ensure(['bug_117'], function(require){
+    ok(true, "require.ensure() called with moduleList as an Array didn't throw an exception");
+    start();
+  });
+});
+
+test("#117 require.ensure() - if first argument (moduleList) is an Object, an exception should be thrown", 1, function() {
+  require.setModuleRoot('/tests/amd/includes/bugs');
+  raises(function() {
+    require.ensure({}, function(require){});
+  });
+});
+
+test("#117 require.ensure() - if first argument (moduleList) is a Number, an exception should be thrown", 1, function() {
+  require.setModuleRoot('/tests/amd/includes/bugs');
+  raises(function() {
+    require.ensure(1, function(require){});
+  });
+});
+
+test("#117 require.ensure() - if first argument (moduleList) is a String, an exception should be thrown", 1, function() {
+  require.setModuleRoot('/tests/amd/includes/bugs');
+  raises(function() {
+    require.ensure("invalid moduleList", function(require){});
+  });
+});
+
+test("#117 require.ensure() - if first argument (moduleList) is null, an exception should be thrown", 1, function() {
+  require.setModuleRoot('/tests/amd/includes/bugs');
+  raises(function() {
+    require.ensure(null, function(require){});
+  });
+});
+
+test("#117 require.ensure() - if first argument (moduleList) is undefined, an exception should be thrown", 1, function() {
+  require.setModuleRoot('/tests/amd/includes/bugs');
+  raises(function() {
+    var moduleList;
+    require.ensure(moduleList, function(require){});
   });
 });
