8be5002636f73e224ff519f0ab775bd65d6decae
Jordan Hoff <jhoff@linkedin.com>
2012-12-21 00:43:28 -0600

    WIP

---
+++ assets/config.rb
@@ -0,0 +1,14 @@
+require 'archetype'
+
+project_type      = :stand_alone
+css_dir           = "css"
+sass_dir          = "_sass"
+images_dir        = "images"
+fonts_dir         = "fonts"
+output_style      = :compressed
+http_images_path  = "/assets/images"
+http_fonts_path   = "/assets/fonts"
+line_comments     = false
+environment       = :development
+
+disable_warnings = true
