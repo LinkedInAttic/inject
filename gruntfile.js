67ebead2547aab75c7ebd5f48d947166e290041f
Jakob Heuser <jakob@felocity.com>
2013-05-20 10:18:21 -0700

    adding release code for gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -23,6 +23,8 @@ module.exports = function (grunt) {
 
     // these are the possible output files when done
     output_files: {
+      all:          './artifacts/inject-__INJECT__VERSION__/*',
+      all_cwd:      './artifacts/inject-__INJECT__VERSION__/',
       main:         './artifacts/inject-__INJECT__VERSION__/inject.js',
       main_min:     './artifacts/inject-__INJECT__VERSION__/inject.min.js',
       ie7:          './artifacts/inject-__INJECT__VERSION__/inject-ie7.js',
@@ -55,6 +57,10 @@ module.exports = function (grunt) {
      * shell: run shell commands. We use this for git ops
      */
     shell: {
+      commitDist: {
+        command: 'git add dist && git commit -m "auto-commit inject release via gruntfile"',
+        options: {}
+      },
       tag: {
         command: 'git describe HEAD',
         options: {
@@ -125,6 +131,11 @@ module.exports = function (grunt) {
         files: [
           {src: ['./src/xd/relay.html'], dest: '<%= output_files.relayHtml %>', filter: 'isFile'},
           {src: ['./src/xd/relay.swf'], dest: '<%= output_files.relaySwf %>', filter: 'isFile'}
+        ]
+      },
+      release: {
+        files: [
+          {expand:true, cwd: '<%= output_files.all_cwd %>', src: '*', dest: './dist/', filter: 'isFile'}
         ]
       }
     },
